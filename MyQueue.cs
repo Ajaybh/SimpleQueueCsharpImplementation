03c64e06db0920fa68fe3332e41ead9b6a5f9d98
Ajay <ajayblp@gmail.com>
2021-11-17 17:35:52 -0500

    Update MyQueue.cs

--- MyQueue.cs
+++ MyQueue.cs
@@ -1,40 +1,76 @@
+using System;
+
+namespace SimpleQueue
+{
     public class MyQueue
     {
         private int[] _repository = new int[1000];
-        private int head = -1;
-        private int tail = 0;
+        private int _head = -1;
+        private int _tail = 0;
         public MyQueue()
-        {}
+        { }
 
         public void Enqueue(int input)
         {
-            if (tail >= _repository.Length - 1)
+            if (_tail >= _repository.Length - 1)
             {
                 throw new Exception("Queue is full");
             }
-            _repository[tail] = input;
-            tail++;
-            if(head == -1)
+            _repository[_tail] = input;
+            _tail++;
+            if (_head == -1)
             {
-                head = 0;
+                _head = 0;
             }
         }
 
         public int Dequeue()
         {
-            if (head == -1 || head > tail)
+            if (_head == -1)
             {
-                head = -1;
-                tail = 0;
                 throw new Exception("Queue is empty");
             }
-            var result = _repository[head];
-            head++;
+            var result = _repository[_head];
+            _head++;
+            if(_head > _tail)
+            {
+                _head = -1;
+                _tail = 0;
+            }
             return result;
         }
 
         public int Size()
         {
-            return tail - head;
+            return _tail - _head;
         }
     }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyQueue myQueue = new MyQueue();
+            myQueue.Enqueue(10);
+            myQueue.Enqueue(15);
+            Console.WriteLine(myQueue.Dequeue());
+            Console.WriteLine(myQueue.Dequeue());
+            myQueue.Enqueue(20);
+            Console.WriteLine(myQueue.Dequeue());
+            myQueue.Enqueue(30);
+            Console.WriteLine(myQueue.Dequeue());
+            Console.WriteLine(myQueue.Dequeue());
+            try
+            {
+                Console.WriteLine(myQueue.Dequeue());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            myQueue.Enqueue(40);
+            Console.WriteLine(myQueue.Dequeue());
+            Console.WriteLine(myQueue.Size());
+            Console.ReadLine();
+        }
+    }
+}
